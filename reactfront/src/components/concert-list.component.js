a1aed06da7e514a7e1bc4f259690d2c8ad59e32e
Beives <38698358+Beives@users.noreply.github.com>
2021-01-26 16:38:21 +0100

    created delete and list function

--- reactfront/src/components/concert-list.component.js
+++ reactfront/src/components/concert-list.component.js
@@ -2,8 +2,22 @@ import React,{Component} from 'react';
 import axios from 'axios';
 import { Link } from 'react-router-dom';
 
+const Concert = props => {
+    return(
+        <tr>
+        <td>{props.concert.zenekar}</td>
+        <td>{props.concert.ido}</td>
+        <td>{props.concert.szinpad}</td>
+        <td>{props.concert.tomeg}</td>
+        <td>
+            <Link to={"/edit/"+props.concert.id}>Módosítás</Link> | <a href="#" onClick={() => { props.deleteConcert(props.concert.id) }}>Törlés</a>
+        </td>
+    </tr>
+    )
+}
+
 export default class ConcertList extends Component{
-    constructor(prosp){
+    constructor(props){
         super(props);
 
         this.deleteConcert = this.deleteConcert.bind(this);
@@ -21,11 +35,40 @@ export default class ConcertList extends Component{
         })
     }
 
-    render(){
-        return(
-            <div>
-                lista
-            </div>
-        )
+    deleteConcert(id){
+        axios.delete('http://localhost:5000/'+id)
+        .then(res => console.log(res.data+" "+id));
+        this.setState({
+            concerts: this.state.concerts.filter(el=>el.id !== id)
+        })
     }
+
+    concertList(){
+        return this.state.concerts.map(currentConcert => {
+            return <Concert concert={currentConcert} deleteConcert={this.deleteConcert} key={currentConcert._id}/>;
+          })
+    }
+
+    render() {
+        return (
+          <div>
+            <h3>Koncertek</h3>
+            <table className="table">
+              <thead className="thead-light">
+                <tr>
+                  <th>Zenekar</th>
+                  <th>Idősáv</th>
+                  <th>Helyszín</th>
+                  <th>Közönség méret</th>
+                  <th>Kezelés</th>
+                </tr>
+              </thead>
+              <tbody>
+                { this.concertList() }
+              </tbody>
+            </table>
+          </div>
+        )
+      }
+
 }
--- reactfront/src/components/navbar.component.js
+++ reactfront/src/components/navbar.component.js
@@ -8,15 +8,14 @@ export default class Navbar extends Component {
       <nav className="navbar navbar-dark bg-dark navbar-expand-lg">
         <Link to="/" className="navbar-brand">Koncertek</Link>
         <div className="collpase navbar-collapse">
-        <ul className="navbar-nav mr-auto">
-          <li className="navbar-item">
-          <Link to="/" className="nav-link">Lista</Link>
-          </li>
-          <li className="navbar-item">
-          <Link to="/create" className="nav-link">Koncert felvétele</Link>
-          </li>
-
-        </ul>
+            <ul className="navbar-nav mr-auto">
+                <li className="navbar-item">
+                <Link to="/" className="nav-link">Lista</Link>
+                </li>
+                <li className="navbar-item">
+                <Link to="/create" className="nav-link">Koncert felvétele</Link>
+                </li>
+            </ul>
         </div>
       </nav>
     );
